adcde35f7d7d03702d9aa500fcf4e98a5d51cb46
hai0z <hainguyen56211@gmail.com>
2023-05-15 23:14:22 +0700

    fix

--- lib/prisma/task.ts
+++ lib/prisma/task.ts
@@ -1,5 +1,4 @@
 "use server";
-import { Task } from "@/app/page";
 import prisma from ".";
 import { revalidatePath } from "next/cache";
 
@@ -7,16 +6,16 @@ export const getAllTask = async () => {
     const task = await prisma.task.findMany({});
     return task;
 };
-export const createTask = async (task: Task) => {
-    await prisma.task.create({
-        data: task,
-    });
-};
-export const deleteTask = async (id: string | undefined) => {
-    await prisma.task.delete({
-        where: {
-            id,
-        },
-    });
-    revalidatePath("/");
-};
+// export const createTask = async (task: Task) => {
+//     await prisma.task.create({
+//         data: task,
+//     });
+// };
+// export const deleteTask = async (id: string | undefined) => {
+//     await prisma.task.delete({
+//         where: {
+//             id,
+//         },
+//     });
+//     revalidatePath("/");
+// };
