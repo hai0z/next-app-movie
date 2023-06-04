8bcee86e5400ce1be985b2466217275dcbe81605
hai0z <hainguyen56211@gmail.com>
2023-06-04 16:54:14 +0700

    fix cai nhe

--- hooks/useWindowDemensions.tsx
+++ hooks/useWindowDemensions.tsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
 function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
+    if (typeof window !== "undefined") {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+            width,
+            height,
+        };
+    }
     return {
-        width,
-        height,
+        width: 0,
+        height: 0,
     };
 }
 
